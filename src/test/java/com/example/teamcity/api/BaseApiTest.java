f7e8c9da3b557586ace6ccbd858505cc72f841d6
Elena Bogacheva <ebogacheva@yahoo.com>
2025-04-10 22:22:42 +0300

    first cicd steps

--- src/test/java/com/example/teamcity/api/BaseApiTest.java
+++ src/test/java/com/example/teamcity/api/BaseApiTest.java
@@ -1,6 +1,37 @@
 package com.example.teamcity.api;
 
 import com.example.teamcity.BaseTest;
+import com.example.teamcity.api.models.AuthModules;
+import com.example.teamcity.api.models.ServerAuthSettings;
+import com.example.teamcity.api.requests.ServerAuthRequest;
+import com.example.teamcity.api.spec.Specifications;
+import org.junit.platform.suite.api.AfterSuite;
+import org.junit.platform.suite.api.BeforeSuite;
+
+import static com.example.teamcity.api.generators.TestDataGenerator.generate;
 
 public class BaseApiTest extends BaseTest {
+    private final ServerAuthRequest serverAuthRequest = new ServerAuthRequest(Specifications.superUserAuthSpec());
+    private AuthModules authModules;
+    private boolean perProjectPermissions;
+
+    @BeforeSuite
+    public void setUpServerAuthSettings() {
+        perProjectPermissions = serverAuthRequest.read().getPerProjectPermissions();
+
+        authModules = generate(AuthModules.class);
+
+        serverAuthRequest.update(ServerAuthSettings.builder()
+                        .perProjectPermissions(true)
+                        .modules(authModules)
+                .build());
+    }
+
+    @AfterSuite
+    public void cleanUpServerAuthSettings() {
+        serverAuthRequest.update(ServerAuthSettings.builder()
+                .perProjectPermissions(perProjectPermissions)
+                .modules(authModules)
+                .build());
+    }
 }
