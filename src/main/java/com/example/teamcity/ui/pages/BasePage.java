7077839287caeb0885c4951656d00eebefce320a
Elena Bogacheva <ebogacheva@yahoo.com>
2025-04-11 23:42:47 +0300

    teamcity setup test added

--- src/main/java/com/example/teamcity/ui/pages/BasePage.java
+++ src/main/java/com/example/teamcity/ui/pages/BasePage.java
@@ -4,4 +4,5 @@ import java.time.Duration;
 
 public abstract class BasePage {
     protected static final Duration BASE_WAITING = Duration.ofSeconds(30);
+    protected static final Duration LONG_WAITING = Duration.ofMinutes(3);
 }
