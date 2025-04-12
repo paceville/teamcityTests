81c60acd0dbda3c392bf3669dc5eff7fe143ab83
Elena Bogacheva <ebogacheva@yahoo.com>
2025-04-13 01:14:07 +0300

    First pages commit

--- src/main/java/com/example/teamcity/ui/pages/BasePage.java
+++
@@ -1,8 +0,0 @@
-package com.example.teamcity.ui.pages;
-
-import java.time.Duration;
-
-public abstract class BasePage {
-    protected static final Duration BASE_WAITING = Duration.ofSeconds(30);
-    protected static final Duration LONG_WAITING = Duration.ofMinutes(3);
-}
